f058fabd08415fbd9d2240f1f147f4ddc552a293
Alfie Wilmot <alfred.wilmot@students.plymouth.ac.uk>
2019-03-20 15:21:21 +0000

    added package for testing single antagonist joint, using Arduino Micro

---
+++ catkin_ws/src/arduino_antagonist_joint_controller/src/antagonist_joint_control/antagonist_joint_control.ino
@@ -0,0 +1,101 @@
+
+
+  // Arduino UNO resources...
+  // PWM duty mapping : https://www.theengineeringprojects.com/2017/03/use-arduino-pwm-pins.html
+  // pin-out:           https://www.circuito.io/blog/arduino-uno-pinout/
+
+  
+  /* Allocating Joystick pins */
+  static int joystick_pitch_pin = 14;
+  static int joystick_yaw_pin   = 15;
+  static int joystick_SW_pin    = 2;
+
+  pinMode(joystick_pitch_pin, INPUT);
+  pinMode(joystick_yaw_pin,   INPUT);
+  pinMode(joystick_SW_pin,    INPUT);
+
+  /* L298N control pins (Set these LO to perform free-running stop) */
+  static int EN_A = 11;  // (pwm) 
+  static int EN_B = 10;  // (pwm)
+
+  pinMode(EN_A, OUTPUT);
+  pinMode(EN_B, OUTPUT);
+
+  /* Pin pairs define motor rotation direction (Set pairs equal, if corresponding EN is HI, to perform hard-stop) */
+  static int IN_1 = 9;
+  static int IN_2 = 8;
+  static int IN_3 = 7;
+  static int IN_4 = 6;
+
+  pinMode(IN_1, OUTPUT);
+  pinMode(IN_2, OUTPUT);
+  pinMode(IN_3, OUTPUT);
+  pinMode(IN_4, OUTPUT);
+
+  static int EN_A_DUTY = 0;
+  static int EN_B_DUTY = 0;
+
+/* Fcn for controlling motor given joystick input */
+void move_motor(int duty, int IN_A, int IN_B, int EN)
+{
+  /* Joystick is in resting position when analogRead(joystick_x_pin) equals ~ 512 +/- 5
+      If below this value, go bkwrds (IN_1 = LO ; IN_2 = HI).
+      If above this value, go fwds   (IN_1 = HI ; IN_2 = LO).
+      Else, turn PWM off gently      (EN_A = LO).
+  */
+  int joy_rest      = 512;  // Value of joystick when untouched.
+  int thresh        = 20;    // Hysteresis around joystick resting value
+  int thresh_lower  = joy_rest - thresh;
+  int thresh_upper  = joy_rest + thresh;
+
+
+  
+  if(duty >= thresh_upper)
+  {
+    /* Go fwd */
+    digitalWrite(IN_A, HIGH);
+    digitalWrite(IN_B, LOW);
+
+    /* Map range [thresh_upper, 1023] to [0, 255] */
+    duty = map(duty, thresh_upper, 1023, 0, 255);
+    analogWrite(EN, duty);
+
+  }
+  else if(duty <= thresh_lower)
+  {
+    /* Go bkwds */
+    digitalWrite(IN_A, LOW);
+    digitalWrite(IN_B, HIGH);
+
+    /* Map range [0, thresh_lower] to [255, 0] */
+    duty = map(duty, 0, thresh_lower, 255, 0);
+    analogWrite(EN, duty);
+
+  }
+  /* Gentle stop => PWM_duty = 0% */
+  else
+  {
+    analogWrite(EN, 0);
+  }
+  
+}
+
+/* Move motors according to joystick position (Antagonist pairs simply move in opposite direction for now) */
+void test_joy_stick()
+{
+  move_motor(analogRead(joystick_yaw_pin),    IN_1, IN_2, EN_A);
+  move_motor(analogRead(joystick_pitch_pin),  IN_3, IN_4, EN_B);
+}
+
+
+void setup() {
+  /* Arduino Bureaucracy, soon I shall be free from it's thrall... */
+}
+
+void loop() {
+  while(1)
+  {
+    test_joy_stick();
+    _delay_ms(20);
+  }
+}
