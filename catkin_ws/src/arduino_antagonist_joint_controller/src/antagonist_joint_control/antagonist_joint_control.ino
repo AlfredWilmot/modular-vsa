3eab86e26fafd6a3dcec4596821d3a74f0093161
Alfie Wilmot <alfred.wilmot@students.plymouth.ac.uk>
2019-03-24 00:07:08 +0000

    successful open-loop control of single segment joint while using xbox360 controller

--- catkin_ws/src/arduino_antagonist_joint_controller/src/antagonist_joint_control/antagonist_joint_control.ino
+++ catkin_ws/src/arduino_antagonist_joint_controller/src/antagonist_joint_control/antagonist_joint_control.ino
@@ -3,61 +3,7 @@
 #define USE_USBCON 
 
 #include <ros.h>
-#include <sensor_msgs/Joy.h>
-
-
-
-/* Fcn for controlling motor given joystick input */
-void move_motor(int duty, int IN_A, int IN_B, int EN)
-{
-  /* Joystick is in resting position when analogRead(joystick_x_pin) equals ~ 512 +/- 5
-      If below this value, go bkwrds (IN_1 = LO ; IN_2 = HI).
-      If above this value, go fwds   (IN_1 = HI ; IN_2 = LO).
-      Else, turn PWM off gently      (EN_A = LO).
-  */
-  int joy_rest      = 0;  // Value of joystick when untouched.
-  int thresh        = 0.1;    // Hysteresis around joystick resting value
-  int thresh_lower  = joy_rest - thresh;
-  int thresh_upper  = joy_rest + thresh;
-
-
-
-  if(duty >= thresh_upper)
-  {
-    /* Go fwd */
-    digitalWrite(IN_A, HIGH);
-    digitalWrite(IN_B, LOW);
-
-    /* Map range [thresh_upper, 1023] to [0, 255] */
-    duty = map(duty, thresh_upper, 1023, 0, 255);
-    analogWrite(EN, duty);
-
-  }
-  else if(duty <= thresh_lower)
-  {
-    /* Go bkwds */
-    digitalWrite(IN_A, LOW);
-    digitalWrite(IN_B, HIGH);
-
-    /* Map range [0, thresh_lower] to [255, 0] */
-    duty = map(duty, 0, thresh_lower, 255, 0);
-    analogWrite(EN, duty);
-
-  }
-  /* Gentle stop => PWM_duty = 0% */
-  else
-  {
-    analogWrite(EN, 0);
-  }
-
-}
-
-
-
-
-
-
-
+#include "std_msgs/UInt8MultiArray.h"
 
 
   /// Arduino Micro resources...
@@ -81,30 +27,18 @@ const int EN_B_DUTY = 0;
 
 
 /* Joystick subscriber callback for controlling motors of one joint */
-void test_proximal_joint(const sensor_msgs::Joy& msg)
+void test_proximal_joint(const std_msgs::UInt8MultiArray& msg)
 {
-    bool A_btn  = msg.buttons[0];
 
-    bool B_btn  = msg.buttons[1];
-    bool X_btn  = msg.buttons[2];
-
-    bool LB_btn = msg.buttons[4];
-    bool RB_btn = msg.buttons[5];
-
-    int duty = 100; //duty: 0,255
-
-    //Right motor rotates:  CW if A is pressed or if B is pressed:                 A || B
-    //                      CCW if A && RB are pressed or if B && RB are pressed:  RB && (A || B)
-
-    //Left motor roates:    CW if A is pressed or if X is pressed:                 A || X
-    //                      CCW if A && LB are pressed or if X && LB are pressed:  LB && (A || X)
+    int left_motor_dir   = msg.data[0];
+    int left_motor_duty  = msg.data[1];
+    int right_motor_dir  = msg.data[2];
+    int right_motor_duty = msg.data[3];
+    
 
 
-
-    /* CONTROL RIGHT MOTOR */
-    if(A_btn || B_btn)
-    {
-      if(RB_btn)
+ 
+      if(left_motor_dir)
       {
         // FWD?
         digitalWrite(IN_1, HIGH);
@@ -117,19 +51,11 @@ void test_proximal_joint(const sensor_msgs::Joy& msg)
         digitalWrite(IN_2, HIGH);
       }
 
-      analogWrite(EN_A, duty);
-    }
-    else
-    {
-        /* STOP */
-        analogWrite(EN_A, 0);
-    }
+      analogWrite(EN_A, left_motor_duty);
 
-    /* CONTROL LEFT MOTOR */
-    if(A_btn || X_btn)
-    {
 
-      if(LB_btn)
+
+      if(right_motor_dir)
       {
         // FWD?
         digitalWrite(IN_3, HIGH);
@@ -142,19 +68,13 @@ void test_proximal_joint(const sensor_msgs::Joy& msg)
         digitalWrite(IN_4, HIGH);
       }
 
-      analogWrite(EN_B, duty);
-    }
-    else
-    {
-        /* STOP */
-        analogWrite(EN_B, 0);
-    }
+      analogWrite(EN_B, right_motor_duty);
 }
 
 
 
 ros::NodeHandle nh;
-ros::Subscriber<sensor_msgs::Joy> sub("joy", &test_proximal_joint);
+ros::Subscriber<std_msgs::UInt8MultiArray> sub("/segment_motor_cmds", &test_proximal_joint);
 
 void setup() {
 
@@ -178,6 +98,6 @@ void setup() {
 void loop() {
 
   nh.spinOnce();
-  delay(10);
+  delay(1);
 
 }
