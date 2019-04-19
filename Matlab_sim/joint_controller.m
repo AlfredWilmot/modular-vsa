5467f4da4224b3797ca96d186003a6689fcee444
AlfredWilmot <alfred.wilmot@students.plymouth.ac.uk>
2019-04-19 23:27:03 +0100

    equation for motor velocity to drive joint from one position to another (consider fitting sinusoid to this-- i.e. peak acceleration when half-way to target)

--- Matlab_sim/joint_controller.m
+++ Matlab_sim/joint_controller.m
@@ -9,7 +9,7 @@ clear all;
 % -> motor-shaft diameter is constant (this is in fact a function of joint
 %       position as currently the cable will spool over itself).
 
-% Comparing the system response between two derivations, starting from resting position (theta_0)
+%% Comparing the system response between two derivations, starting from resting position (theta_0)
 % moving towards all possible joint positoins (theta_1) ranging from -pi/4
 % to pi/4.
 theta_0 = 0;
@@ -40,3 +40,36 @@ legend("deta_l","delta_R");
 title("Approximately linear-response of cable-length variations");
 xlabel("desired joint angle offset from 0 degrees (deg)");
 ylabel("change in cable-length function output (mm)");
+
+
+
+
+
+%% What motor velocity model is needed in order to efficiently drive joint towards desired position.
+
+clc;
+close all;
+clear all;
+
+
+% make recursive function that compares "current" joint angle against
+% "target" joint angle?
+
+% dummy set of "current" joint values as they go from 0 to theta_1.
+theta_0 = [-pi/4:0.01:pi/4];
+
+% simulate time steps.
+delta_t = 0.1;
+
+% the "desired" joint angle.
+theta_1 = theta_0(length(theta_0));
+
+k = 1;
+R_0 = (2^0.5)* k * (1 - cos((pi/4) + theta_0)).^0.5;
+R_1 = (2^0.5)* k * (1 - cos((pi/4) + theta_1)).^0.5;
+
+v_motor = -(R_0 - R_1)/delta_t;
+
+plot(theta_0,v_motor);
+
+
