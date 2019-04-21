c6921637d4841a91f8407c6379d78856075508ab
Alfie Wilmot <alfred.wilmot@students.plymouth.ac.uk>
2019-04-21 18:30:44 +0100

    tested current sensing using stm-bluepill, test ROS integration

--- STM32F103C8T6/stm32_printf_test_01/src/main.cpp
+++ STM32F103C8T6/stm32_printf_test_01/src/main.cpp
@@ -1,25 +1,32 @@
 #include <mbed.h>
 
 #define LED_BUILTIN PC_13
-#define SERIAL2_TX PA_2
-#define SERIAL2_RX PA_3
+#define SERIAL1_TX PB_6
+#define SERIAL1_RX PB_7
 
-Serial serial2(SERIAL2_TX, SERIAL2_RX, 115200);
+Serial serial1(SERIAL1_TX, SERIAL1_RX, 115200);
 DigitalOut led(LED_BUILTIN);
+AnalogIn I_sense_1(PB_1);
+AnalogIn I_sense_2(PB_0);
+
+
 
 int main()
 {
     // put your setup code here, to run once:
-    serial2.printf("STM32 bluepill mbed test.\n");
+    serial1.printf("STM32 bluepill mbed test.\n");
 
     while (1)
     {
-        // put your main code here, to run repeatedly:
-        led = 0;
-        wait_ms(500);
-        led = 1;
-        wait_ms(500);
 
-        serial2.printf("Blinking...\n");
+        led = 1;
+        wait_ms(5);
+
+        led = 0;
+        wait_ms(5);
+
+        serial1.printf("I-sense 1: %d\n\r", I_sense_1.read_u16());
+        serial1.printf("I-sense 2: %d\n\r", I_sense_2.read_u16());
+
     }
 }
