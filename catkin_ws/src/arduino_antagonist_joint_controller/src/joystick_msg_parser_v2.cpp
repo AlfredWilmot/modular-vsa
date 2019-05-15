5d150772da38a7de9ee12c301e7d0690468897f3
Alfie Wilmot <alfred.wilmot@students.plymouth.ac.uk>
2019-05-16 00:27:09 +0100

    simple CL controller (no speed control atm, just dir) for proximal joint

--- catkin_ws/src/arduino_antagonist_joint_controller/src/joystick_msg_parser_v2.cpp
+++ catkin_ws/src/arduino_antagonist_joint_controller/src/joystick_msg_parser_v2.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
 #include "std_msgs/UInt16MultiArray.h"
+#include "std_msgs/UInt16.h"
 #include "std_msgs/UInt8.h"
 #include "std_msgs/Bool.h"
 #include "string.h"
@@ -12,15 +13,20 @@
 /* Function declarations */
 /*----------------------*/
 
-//Subscriber callback for Open-Loop control
+//Subscriber callback for Open-Loop control using gamepad
 void OL_control(const sensor_msgs::JoyConstPtr& msg);
 
+//Closed-loop control for maintaining current position
+void CL_control();
+void reset_all_motors();
+
 // Functions to directly control joint using D-pad & co-contract with left-trigger.
-void proximal_joint_cmds(const sensor_msgs::JoyConstPtr msg);
-void distal_joint_cmds(const sensor_msgs::JoyConstPtr msg);
+int proximal_joint_cmds(const sensor_msgs::JoyConstPtr msg);
+int distal_joint_cmds(const sensor_msgs::JoyConstPtr msg);
 
 // Prevent duplicate control packets from being sent to MCU, and also prevents expired packages from persisting.
 int filter_duplicate_packets();
+
 
 /*------------*/
 /* motor msg */
@@ -64,13 +70,21 @@ const int A_btn             = 0;
 
 
 
-/*--------------------------------------------*/
-/* global variables for the joint controller */
-/*------------------------------------------*/
+/*-------------------------------------------------------------------------------*/
+/* global variables for the joint controller, and relevant subscriber callbacks */
+/*-----------------------------------------------------------------------------*/
 
 // Encoder values.
-static int proximal_joint_value;
-static int distal_joint_value;
+static int initial_proximal_joint_value = 0;
+
+static int initial_distal_joint_value = 0;
+
+static int proximal_joint_value = 0;
+static int distal_joint_value = 0;
+
+// Encoder subscriber callbacks 
+void grab_proximal_encoder_value(const std_msgs::UInt16ConstPtr msg);
+void grab_distal_encoder_value(const std_msgs::UInt16ConstPtr msg);
 
 // I-sense values.
 static int proximal_motorA_load;
@@ -78,12 +92,28 @@ static int proximal_motorB_load;
 static int distal_motorA_load;
 static int distal_motorB_load;
 
+// I-sense subscriber callbacks
+void grab_proximal_motorA_load(const std_msgs::UInt16ConstPtr msg);
+void grab_proximal_motorB_load(const std_msgs::UInt16ConstPtr msg);
+void grab_distal_motorA_load(const std_msgs::UInt16ConstPtr msg);
+void grab_distal_motorB_load(const std_msgs::UInt16ConstPtr msg);
+
+
+// Various experimental states controlled by gamepad toggle-switches.
+bool CL_control_mode_is_active = false;
+bool D_pad_in_use = false;
+
+
+
 
 /* Subscribe to the xbox-360 joy-stick topic, 
    publish corresponding motor control msg to "/segment_motor_cmds" topic */
+/*------------*/
+/* Main Loop */
+/*----------*/
 int main(int argc, char **argv)
 {
-    /* Initializing motor packet*/ 
+    /* Initializing motor packet */ 
     motor_packet.data.clear();
     previous_motor_packet.data.clear();
 
@@ -101,21 +131,78 @@ int main(int argc, char **argv)
 
     /* Subscribe to gamepad /joy topic */
     ros::Subscriber sub = nh.subscribe("joy", 1, OL_control);
-   
+    
+    ros::Subscriber proximal_encoder_sub    = nh.subscribe("proximal_encoder", 1, grab_proximal_encoder_value);
+    ros::Subscriber distal_encoder_sub      = nh.subscribe("distal_encoder",   1, grab_distal_encoder_value);
+
+    ros::Subscriber proximal_motorA_load    = nh.subscribe("I_sense_1", 1, grab_proximal_motorA_load);
+    ros::Subscriber proximal_motorB_load    = nh.subscribe("I_sense_3", 1, grab_proximal_motorB_load);
+    ros::Subscriber distal_motorA_load      = nh.subscribe("I_sense_2", 1, grab_distal_motorA_load);
+    ros::Subscriber distal_motorB_load      = nh.subscribe("I_sense_4", 1, grab_distal_motorB_load);
    
     /* Publish filtered joy-msg into binary values that will be allocated by MCU as digital pin output states (thereby controlling motor direction) */
     motor_pub = nh.advertise<std_msgs::UInt16MultiArray>("segment_motor_cmds", 1);
 
-    /* Service any subscriber callbacks (publishing parsed data takes place within callback) */
-    ros::spin();
 
-    return 0;
+    ros::Rate loop_rate(50); //10Hz loop rate
+
+    while(ros::ok())
+    {
+        
+        if(CL_control_mode_is_active && !D_pad_in_use) // if gamepad is not in use, and the CL control mode has been activated, run CL control to hold current position.
+        {
+            CL_control();
+        }
+
+        /* Service any subscriber callbacks (publishing parsed data takes place within callback) */
+        ros::spinOnce();
+
+        /* Enforces loop period to the designated rate (only works properly if all processes preceeding sleep() have a lower latency than 1/loop_rate). */
+        loop_rate.sleep();
+    }
+    
+
+    return 0; // exit when roscore dies
 }
 
 
 
+/* Some motor direction functions to enhance code-suability (just prepare packet, need to publish externally)*/
+void tilt_proximal_joint_down()
+{
+        //proximal joint: down
+        motor_packet.data.at(4) = 0;
+        motor_packet.data.at(5) = 1;
+        motor_packet.data.at(6) = 0;
+        motor_packet.data.at(7) = 1;
+}
+void tilt_proximal_joint_up()
+{
+        //proximal joint: up
+        motor_packet.data.at(4) = 1;
+        motor_packet.data.at(5) = 0;
+        motor_packet.data.at(6) = 1;
+        motor_packet.data.at(7) = 0;
+}
+void tilt_distal_joint_left()
+{
+    //distal_joint: left
+    motor_packet.data.at(0) = 1;
+    motor_packet.data.at(1) = 0;
+    motor_packet.data.at(2) = 1;
+    motor_packet.data.at(3) = 0;
+}
+void tilt_distal_joint_right()
+{
+    //distal joint: right
+    motor_packet.data.at(0) = 0;
+    motor_packet.data.at(1) = 1;
+    motor_packet.data.at(2) = 0;
+    motor_packet.data.at(3) = 1;
+}
+
 /* Define Proximal joint movement */
-void proximal_joint_cmds(const sensor_msgs::JoyConstPtr msg)
+int proximal_joint_cmds(const sensor_msgs::JoyConstPtr msg)
 {
 
     if(msg->axes[d_pad_up_down] == 1.0)
@@ -167,12 +254,16 @@ void proximal_joint_cmds(const sensor_msgs::JoyConstPtr msg)
         motor_packet.data.at(4) = 0;
         motor_packet.data.at(5) = 0;
         motor_packet.data.at(6) = 0;
-        motor_packet.data.at(7) = 0;         
+        motor_packet.data.at(7) = 0;     
+
+        return 0;   //indicate D-pad not pressed 
     }
+
+    return 1;       // indicate D-pad pressed 
 }
 
 /* Define Distal joint movement */
-void distal_joint_cmds(const sensor_msgs::JoyConstPtr msg)
+int distal_joint_cmds(const sensor_msgs::JoyConstPtr msg)
 {
 
     if(msg->axes[d_pad_left_right] == 1.0)
@@ -225,8 +316,13 @@ void distal_joint_cmds(const sensor_msgs::JoyConstPtr msg)
         motor_packet.data.at(0) = 0;
         motor_packet.data.at(1) = 0;
         motor_packet.data.at(2) = 0;
-        motor_packet.data.at(3) = 0;   
+        motor_packet.data.at(3) = 0;  
+
+        return 0;   //indicate D-pad not pressed 
     }
+
+    return 1;       //indicate D-pad pressed 
+
 }
 
 
@@ -257,19 +353,88 @@ int filter_duplicate_packets()
        
 }
 
+/* Put all motors into brake mode */
+void reset_all_motors()
+{
+    for(int i=0; i<packet_length; i++)
+    {   
+       motor_packet.data.at(i) = 0;
+    }
+
+    motor_pub.publish(motor_packet);
+}
+
+/*--------------------------------*/
+/* Subscriber callback functions */
+/*------------------------------*/
+
+// Encoder subscriber callbacks (grab data frames)
+void grab_proximal_encoder_value(const std_msgs::UInt16ConstPtr msg)
+{
+    if (initial_proximal_joint_value == 0) 
+    {
+        initial_proximal_joint_value = msg->data;
+    }
+
+    proximal_joint_value = msg->data;
+
+}
+
+void grab_distal_encoder_value(const std_msgs::UInt16ConstPtr msg)
+{
+    if (initial_distal_joint_value == 0) 
+    {
+        initial_distal_joint_value = msg->data;
+    }
+
+    distal_joint_value = msg->data;
+}
+
+
+// I-sense subscriber callbacks (grab data frames)
+void grab_proximal_motorA_load(const std_msgs::UInt16ConstPtr msg)
+{
+    proximal_motorA_load = msg->data;
+}
+
+void grab_proximal_motorB_load(const std_msgs::UInt16ConstPtr msg)
+{
+    proximal_motorB_load = msg->data;
+}
+void grab_distal_motorA_load(const std_msgs::UInt16ConstPtr msg)
+{
+    distal_motorA_load = msg->data;
+}
+void grab_distal_motorB_load(const std_msgs::UInt16ConstPtr msg)
+{
+    distal_motorB_load = msg->data;
+}
+
+
+
 /* A controller that uses Right-joyStick, as well as LT & RT for tension control of either joint */
 void OL_control(const sensor_msgs::JoyConstPtr& msg)
 {   
-    // Control joints with D-pad (& left trigger)
-    proximal_joint_cmds(msg);
-    distal_joint_cmds(msg);
-    
-    // set each motor duty to 80% for OL testing (255 = 100%)
-    motor_packet.data.at(8)  = 204;
-    motor_packet.data.at(9)  = 204;
-    motor_packet.data.at(10) = 204;
-    motor_packet.data.at(11) = 204;
 
+    // Toggle CL control mode by pressing xbox btn.
+    if (msg->buttons[xbox_btn]) 
+    {
+        CL_control_mode_is_active = !CL_control_mode_is_active;
+        reset_all_motors();
+    }
+    
+    // See if operator is using the gamepad (if so, disable CL control, and update reference joint position).
+    if(proximal_joint_cmds(msg) || distal_joint_cmds(msg))
+    {
+        D_pad_in_use = true;
+        ROS_INFO("D-pad in Use!\n");
+    }
+    else
+    {
+        D_pad_in_use = false;
+        ROS_INFO("Look, no hands!\n");
+    }
+    
     // Keep it clean!
     if(filter_duplicate_packets() == 0)
     {
@@ -278,24 +443,9 @@ void OL_control(const sensor_msgs::JoyConstPtr& msg)
 }
 
 
-// TODO:
-
-// -> Use a gamepad btn to cycle throught co-contractions (adjusts I-sense threshold)
-
-// NOTES:
-// -> Idle I-sense reading (no I-draw) ~ 580
-// -> Define arbitrary ranges for three distinct co-contraction values:
-//      * 500
-//      * 350
-//      * 200
-
-
-
-// Can only detect torque values when driving motor
-
-void CL_control(const sensor_msgs::Joy& msg)
+void CL_control()
 {
-    /* Sanity test 1: "Hold initial position" */
+    /* Sanity test 1: "Hold initial position" */ 
 
     // -> Sample the current joint angle.
     // -> If the joint is perturbed, drive the motors such that the joint is verged to.
@@ -303,4 +453,76 @@ void CL_control(const sensor_msgs::Joy& msg)
     // -> Fitler noise/ transients in I-sense data by ignoring values that massively deveiate from the previous value (hysteresis band).
 
 
+   // (1): first, make simple P-controller to hold initial position -- initially assuming no tension variation 
+   
+   // initial_position - current position = err
+
+   // if err >= 0 go CW, else go CCW (correct later if wrong)
+   
+
+
+
+    /*----------------------------*/
+    /* Proximal joint controller */
+    /*--------------------------*/
+
+    float P = 2;
+    int hysteresis_band = 25;
+    
+    int proximal_err  = initial_proximal_joint_value  - proximal_joint_value;
+
+    ROS_INFO("Proximal Err: %d\n", proximal_err);
+
+    // If error is within hysteresis band, ignore it.
+    if ( (hysteresis_band - abs(proximal_err) > 0) ) 
+    {
+        proximal_err = 0;
+    }
+
+
+    float corrective_term = P * float(proximal_err);
+    
+    // tilt proximal joint upwards ("up" depends on orientation of magnet in holder)
+    if (corrective_term < 0.0) 
+    {
+
+        corrective_term = abs(corrective_term + hysteresis_band); //compensate hysteresis offset.
+
+        if(corrective_term >= 255)
+        {
+            corrective_term = 255; // saturate to 255.
+        }
+        tilt_proximal_joint_up();
+        ROS_INFO("GO UP!");
+    }
+    else if(corrective_term > 0.0)
+    {
+    // tilt proximal joint downwards
+
+        corrective_term = abs(corrective_term - hysteresis_band);
+
+        if(corrective_term >= 255)
+        {
+            corrective_term = 255; // saturate to 255.
+        }
+
+        tilt_proximal_joint_down();
+        ROS_INFO("GO Down!");
+    }
+    else
+    {
+    // keep segment steady
+        reset_all_motors();
+    }
+    
+    
+    ROS_INFO("Corrective term: %1.2f\n", corrective_term);
+
+    motor_pub.publish(motor_packet);
+
+    // int distal_err    = initial_distal_joint_value    - distal_joint_value;
+  
+
 }
+
+/**************************************************************************************/
